c2bea295361e593392e02702e3dc0cddc4aaad8c
mehemmedgulmemmedov14-debug <mehemmedgulmemmedov14@gmail.com>
2026-08-11 01:52:12 +0400

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,88 +1,11 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
-import org.lwjgl.glfw.GLFW;
 
 public class ExampleMod implements ModInitializer {
-    public static KeyBinding menuKey;
-    public static boolean aimAssist = false;
-    public static boolean triggerbot = false;
-    public static boolean hitbox = false;
-    public static boolean esp = false;
-
     @Override
     public void onInitialize() {
-        // 'M' düyməsi qeydiyyata alınır
-        menuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.ronerclient.menu",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_M,
-            "category.ronerclient"
-        ));
-
-        // 'M' düyməsinə basıldıqda menyunu açır
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (menuKey.wasPressed()) {
-                if (client.player != null) {
-                    client.setScreen(new CheatMenuScreen());
-                }
-            }
-        });
-    }
-
-    // Menyu Ekranı
-    public static class CheatMenuScreen extends Screen {
-        public CheatMenuScreen() {
-            super(Text.of("RonerClient Menu"));
-        }
-
-        @Override
-        protected void init() {
-            int x = this.width / 2 - 100;
-            int y = this.height / 2 - 60;
-
-            // Aim Assist Düyməsi
-            this.addDrawableChild(ButtonWidget.builder(
-                Text.of("Aim Assist: " + (aimAssist ? "ACIQ" : "QAPALI")),
-                btn -> {
-                    aimAssist = !aimAssist;
-                    btn.setMessage(Text.of("Aim Assist: " + (aimAssist ? "ACIQ" : "QAPALI")));
-                }
-            ).dimensions(x, y, 200, 20).build());
-
-            // Triggerbot Düyməsi
-            this.addDrawableChild(ButtonWidget.builder(
-                Text.of("Triggerbot: " + (triggerbot ? "ACIQ" : "QAPALI")),
-                btn -> {
-                    triggerbot = !triggerbot;
-                    btn.setMessage(Text.of("Triggerbot: " + (triggerbot ? "ACIQ" : "QAPALI")));
-                }
-            ).dimensions(x, y + 25, 200, 20).build());
-
-            // Hitbox Düyməsi
-            this.addDrawableChild(ButtonWidget.builder(
-                Text.of("Hitbox: " + (hitbox ? "ACIQ" : "QAPALI")),
-                btn -> {
-                    hitbox = !hitbox;
-                    btn.setMessage(Text.of("Hitbox: " + (hitbox ? "ACIQ" : "QAPALI")));
-                }
-            ).dimensions(x, y + 50, 200, 20).build());
-
-            // ESP Düyməsi
-            this.addDrawableChild(ButtonWidget.builder(
-                Text.of("ESP: " + (esp ? "ACIQ" : "QAPALI")),
-                btn -> {
-                    esp = !esp;
-                    btn.setMessage(Text.of("ESP: " + (esp ? "ACIQ" : "QAPALI")));
-                }
-            ).dimensions(x, y + 75, 200, 20).build());
-        }
+        // Main initialization
     }
 }
+            
